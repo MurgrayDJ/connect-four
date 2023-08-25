8ae6cc430c308bbd40968b1906fc869db5bdcbdd
murgraydj <murgraydj@gmail.com>
2023-08-24 20:21:28 -0400

    Tests #full? for when board is partially filled
    Also corrects unicode character for use in board. It is now \u25CF.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,9 +11,18 @@ RSpec.describe Board do
       end
     end
 
+    context 'board is partially filled' do
+      it 'should return false' do
+        @board_class.board[0,2] =  "\u25CF"
+        @board_class.board[4,5] =  "\u25CF"
+        @board_class.board[3,1] =  "\u25CF"
+        expect(@board_class.full?).to be(false)
+      end
+    end
+
     context 'board is full' do
       it 'should return true' do
-        @board_class.board.map! {|spot| spot = 1}
+        @board_class.board.map! {|spot| spot = "\u25CF"}
         expect(@board_class.full?).to be(true)
       end
     end
