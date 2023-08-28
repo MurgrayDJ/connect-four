7ae354c374768e2ba0d34f4e663e96223835a075
murgraydj <murgraydj@gmail.com>
2023-08-28 17:09:44 -0400

    Adds function and test for partially filled board

--- lib/board.rb
+++ lib/board.rb
@@ -3,28 +3,29 @@ require 'matrix'
 class Board
   attr_accessor :board
 
-  def initialize(board = Matrix.build(6,7) {0})
+  def initialize(board = Matrix.build(6,7) {" "})
     @board = board
   end
 
   def print_board
-    print " " + ("_" * 27)
+    print "      " + ("_" * 27)
     puts
     @board.row_vectors.each do |row|
+      print "     "
       row.each do |spot|
         print "| #{spot} "
       end
       print "|"
       puts
     end
-    print (" \u0305" * 27)
+    print "     " + (" \u0305" * 27)
     puts
   end
 
   def full?
-    @board.any? {|spot| spot == 0} ? false : true
+    @board.any? {|spot| spot == " "} ? false : true
   end
 end
 
-testBoard = Board.new
-testBoard.print_board
+# testBoard = Board.new
+# testBoard.print_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe Board do
       end
     end
 
+    context 'board is partially full' do
+      it 'should print a partially filled board' do
+        @board_class.board[0,3] =  "\u25CF"
+        @board_class.board[2,5] =  "\u25CF"
+        @board_class.board[1,4] =  "\u25CF"
+        @board_class.board[0,2] =  "\u25CB"
+        @board_class.board[4,5] =  "\u25CB"
+        @board_class.board[3,1] =  "\u25CB"
+        @board_class.print_board
+      end
+    end
   end
 
   describe '#full?' do
