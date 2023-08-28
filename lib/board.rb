90274315e211e91a2e848631d929bf68554a765e
murgraydj <murgraydj@gmail.com>
2023-08-28 10:48:52 -0400

    Adds functionality to print empty board
    Also tests that empty board is printed

--- lib/board.rb
+++ lib/board.rb
@@ -7,7 +7,24 @@ class Board
     @board = board
   end
 
+  def print_board
+    print " " + ("_" * 27)
+    puts
+    @board.row_vectors.each do |row|
+      row.each do |spot|
+        print "| #{spot} "
+      end
+      print "|"
+      puts
+    end
+    print (" \u0305" * 27)
+    puts
+  end
+
   def full?
     @board.any? {|spot| spot == 0} ? false : true
   end
 end
+
+testBoard = Board.new
+testBoard.print_board
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,6 +4,15 @@ require 'matrix'
 RSpec.describe Board do
   before { @board_class = described_class.new }
 
+  describe '#print_board' do
+    context 'board is empty' do
+      it 'should print an empty board' do
+        @board_class.print_board
+      end
+    end
+
+  end
+
   describe '#full?' do
     context 'board is empty' do
       it 'should return false' do
