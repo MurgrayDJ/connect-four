ded7aa03ff747d7731a91129be725a3bc1f274e0
murgraydj <murgraydj@gmail.com>
2023-09-27 17:02:39 -0400

    Updates Spec Helper to redirect console output
    Console output is now written to 2 external files during testing,
    instead of to the console. This keeps the test output clean.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,25 @@
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  
+  ## Prevents writing to console
+  # config.before { allow($stdout).to receive(:write) }
+  
+  ## Sends console writes to external file
+  original_stderr = $stderr
+  original_stdout = $stdout
+  error_path = File.join(File.dirname(__FILE__), 'txt_files/error_output.txt')
+  output_path = File.join(File.dirname(__FILE__), 'txt_files/console_output.txt')
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(error_path, "w") 
+    $stdout = File.open(output_path, "w") 
+  end
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
