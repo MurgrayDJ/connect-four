c47fdcfd39ba73639b85f9c642195d09ef3a6c8f
murgraydj <murgraydj@gmail.com>
2023-09-27 17:13:17 -0400

    Adds test for when player doesn't confirm properly
    The player needs to enter y or n to confirm  or change their name. This
    test ensures that their name won't be confirmed or changed unless they
    do.

--- spec/play_game_spec.rb
+++ spec/play_game_spec.rb
@@ -15,7 +15,15 @@ RSpec.describe PlayGame do
     context 'player2 enters name then changes it' do
       it 'should return the second player 2 name' do
         allow(@game).to receive(:gets).and_return("Mumu\n", "n\n", "Linda\n", "Y\n")
-        expect(@game.get_names(1)).to eq("Linda")
+        expect(@game.get_names(2)).to eq("Linda")
+      end
+    end
+
+    context 'player1 enters invalid text for name confirmation' do
+      it 'should still return the first name after eventual yes' do
+        allow(@game).to receive(:gets).and_return(
+          "Bob\n", "kdyfj\n", "2836%(^*\n", "banana34\n", "Y\n")
+        expect(@game.get_names(1)).to eq("Bob")
       end
     end
   end
